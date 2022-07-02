12fef722771e7bee51d65cb21c53a37ac10ca6fa
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-07-02 07:54:55 +0530

    Updated SCM step

---
+++ Jenkinsbuild.groovy
@@ -0,0 +1,11 @@
+//Declarative pipeline
+pipeline {
+    agent any
+    stages{
+        stage("Checkout SCM")
+        steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/develop']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-auth', url: 'https://github.com/KuruvaSomaSekhar/june-private.git']]])
+                sh "ls -lart ./*"
+            }
+    }
+}

e3224af4adbbce6227e3a166193ad6bcbacdb3d4
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-07-02 07:57:13 +0530

    Updated syntax errors

--- Jenkinsbuild.groovy
+++ Jenkinsbuild.groovy
@@ -2,10 +2,11 @@
 pipeline {
     agent any
     stages{
-        stage("Checkout SCM")
+        stage("Checkout SCM") {
         steps {
                 checkout([$class: 'GitSCM', branches: [[name: '*/develop']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-auth', url: 'https://github.com/KuruvaSomaSekhar/june-private.git']]])
                 sh "ls -lart ./*"
             }
+        }
     }
 }

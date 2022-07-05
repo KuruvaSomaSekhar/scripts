e6040981c191aa020551cb0d29a0bcea2f6b132c
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-07-05 07:36:05 +0530

    Updated deployment job

---
+++ Jenkinsdeploy.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    parameters { string(name: 'PROJECT_NAME', defaultValue: 'build-pipeline', description: '') 
+                 string(name: 'SOURCE_CODE_BRANCH', defaultValue: 'develop', description: '')
+                 string(name: 'BUILD_NUMBER', defaultValue: '10', description: '')
+    
+    }
+    stages {
+        stage("Download artifacts") {
+            step {
+                sh '''
+                 aws s3 ls s3://june22artifacts/${PROJECT_NAME}/${SOURCE_CODE_BRANCH}/${BUILD_NUMBER}/
+                 aws s3 cp s3://june22artifacts/${PROJECT_NAME}/${SOURCE_CODE_BRANCH}/${BUILD_NUMBER}/hello-${BUILD_NUMBER}.war .
+                 ls -l
+                 '''
+            }
+        }
+    }
+}

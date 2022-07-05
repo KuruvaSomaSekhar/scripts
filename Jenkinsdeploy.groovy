fd71caab36e169175f527a0a197590f4613b9a00
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-07-05 07:59:31 +0530

    Updated parameters2

--- Jenkinsdeploy.groovy
+++ Jenkinsdeploy.groovy
@@ -2,9 +2,8 @@ pipeline {
     agent any
     parameters { string(name: 'PROJECT_NAME', defaultValue: 'build-pipeline', description: '') 
                  string(name: 'SOURCE_CODE_BRANCH', defaultValue: 'develop', description: '')
-                 string(name: 'BUILD_NUMBER', defaultValue: '10', description: '')
-                 string(name: 'SERVERS', defaultValue: '10', description: '')
-                 choice(name: 'ENVIRONMENT', choices: ['172.31.23.231', '172.31.31.74'], description: 'ENVIRONMENT')
+                 string(name: 'BUILD_NUMBER', defaultValue: '14', description: '')
+                 choice(name: 'ENVIRONMENT', choices: ['172.31.23.231', '172.31.31.74','172.31.23.231,172.31.31.74'], description: '')
     
     }
     stages {
@@ -20,8 +19,19 @@ pipeline {
         stage("Deploy artifacts"){
             steps {
                 sh """
-                    scp -i /tmp/june22_ohio.pem hello-${BUILD_NUMBER}.war ec2-user@${ENVIRONMENT}:/tmp/
-                    ssh -i /tmp/june22_ohio.pem ec2-user@${ENVIRONMENT} "sudo cp /tmp/hello-${BUILD_NUMBER}.war /var/lib/tomcat/webapps/"
+
+                        IFS=',' read -r -a MYSERVERS <<< $ENVIRONMENT
+                        for IP in ${MYSERVERS[@]}
+                            do
+                                echo "Hello Mr. ${IP}"
+                                echo "Welcome to Jenkins piepline loop"
+                                echo
+                                scp -i /tmp/june22_ohio.pem hello-${BUILD_NUMBER}.war ec2-user@${IP}:/tmp/
+                                ssh -i /tmp/june22_ohio.pem ec2-user@${IP} "sudo cp /tmp/hello-${BUILD_NUMBER}.war /var/lib/tomcat/webapps/"
+                            done
+
+                                #scp -i /tmp/june22_ohio.pem hello-${BUILD_NUMBER}.war ec2-user@${ENVIRONMENT}:/tmp/
+                                #ssh -i /tmp/june22_ohio.pem ec2-user@${ENVIRONMENT} "sudo cp /tmp/hello-${BUILD_NUMBER}.war /var/lib/tomcat/webapps/"
                     """
             }
         }

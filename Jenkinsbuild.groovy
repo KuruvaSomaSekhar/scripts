5694178ae4f96a88c72759ba96cc83b6ed4d9c62
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-07-05 07:07:03 +0530

    added parameters

--- Jenkinsbuild.groovy
+++ Jenkinsbuild.groovy
@@ -1,10 +1,11 @@
 //Declarative pipeline
 pipeline {
     agent any
+    parameters { string(name: 'SOURCE_CODE_BRANCH', defaultValue: 'develop', description: 'Provide source code branch from which you want to build') }
     stages{
         stage("Checkout SCM") {
         steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/develop']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-auth', url: 'https://github.com/KuruvaSomaSekhar/june-private.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/${SOURCE_CODE_BRANCH}']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-auth', url: 'https://github.com/KuruvaSomaSekhar/june-private.git']]])
                 sh "ls -lart ./*"
             }
         }
@@ -16,8 +17,8 @@ pipeline {
         }
         stage("Upload artifacts"){
             steps {
-                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://june22artifacts/${env.JOB_NAME}/develop/${BUILD_NUMBER}/"
-                sh "aws s3 ls s3://june22artifacts/${env.JOB_NAME}/develop/${BUILD_NUMBER}/ "
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://june22artifacts/${env.JOB_NAME}/${SOURCE_CODE_BRANCH}/${BUILD_NUMBER}/"
+                sh "aws s3 ls s3://june22artifacts/${env.JOB_NAME}/${SOURCE_CODE_BRANCH}/${BUILD_NUMBER}/ "
             }
         }
     }
